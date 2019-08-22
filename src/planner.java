828156e0d0d4916f4abc37dda3cdf63c79f1bf88
Matthew Winemiller <seidoc@gmail.com>
2019-08-22 09:56:07 -0700

    Added more questions

--- src/planner.java
+++ src/planner.java
@@ -10,9 +10,23 @@ public class planner {
         System.out.println("Welcome to Vacation Planner!");
         System.out.print("What is your name? ");
         String name = input.nextLine();
+        System.out.print("Nice to meet you " + name + ", where are you travelling to? ");
+        String location = input.nextLine();
+        System.out.println("Great! " + location + " sounds like a great trip");
+        budget(input);
     }
 
     public static void budget(Scanner input){
+        System.out.println("*****************");
+        System.out.println("");
+        System.out.print("How many days are you going to spend travelling? ");
+        int days = input.nextInt();
+        System.out.print("How much money, in USD, are you planning to spend on your trip? ");
+        double money = input.nextDouble();
+        System.out.print("What is the three letter currency symbol for your travel destination? ");
+        String currency = input.next();
+        System.out.print("How many " + currency + " are there in 1 USD? ");
+        double conversion = input.nextDouble();
 
     }
 
@@ -21,6 +35,6 @@ public class planner {
     }
 
     public static void distance(Scanner input){
-        
+
     }
 }
