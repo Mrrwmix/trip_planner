b8c8ecfde222e9f5af77659c0a742db1667c046a
Matthew Winemiller <seidoc@gmail.com>
2019-08-27 21:46:56 -0700

    Finished Java project from edX course

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # trip_planner
+
+## Description
+
+A simple Java app from the Microsoft edX Intro to Java course. It is a vacation planner that takes in user input through the command line. See the example below.
+
+## Example
+
+![Image of command line](screenshot/cmd.png)
--- src/planner.java
+++ src/planner.java
@@ -33,17 +33,32 @@ public class planner {
         String currency = input.next();
         System.out.print("How many " + currency + " are there in 1 USD? ");
         double conversion = input.nextDouble();
+        double budget = money * conversion;
 
-        System.out.println("If you are travelling for" + days + " days, that is the same as " + (days * 24) + " hours or " + (days * 24 * 60) + " minutes");
+        System.out.println("If you are travelling for " + days + " days, that is the same as " + (days * 24) + " hours or " + (days * 24 * 60) + " minutes");
         System.out.println("If you are going to spend $" + money + " USD, that means you can spend up to " + df.format((money / days)) + " per day");
+        System.out.println("Your total budget in " + currency + " is " + budget + ", which is " + df.format(budget/days) + " " + currency + " per day");
+        time(input, df);
 
     }
 
     public static void time(Scanner input, DecimalFormat df){
-
+        System.out.println("*****************");
+        System.out.println("");
+        System.out.print("What is the time difference, in hours, between your home and the destination? ");
+        int timeDiff = input.nextInt();
+        int destinationTimeMidnight = (24 + timeDiff) % 24;
+        int destinationTimeNoon = (12 + timeDiff) % 24;
+        System.out.println("That means that when it is midnight at home it will be " + destinationTimeMidnight + ":00 in your travel destination and when it is noon at home it will be " + destinationTimeNoon + ":00");
+        distance(input, df);
     }
 
     public static void distance(Scanner input, DecimalFormat df){
-
+        System.out.println("*****************");
+        System.out.println("");
+        System.out.print("What is the square area of your destination country in km2? ");
+        double areaKM = input.nextDouble();
+        double areaM = areaKM * 0.386102;
+        System.out.println("In miles2, that is " + df.format(areaM));
     }
 }
